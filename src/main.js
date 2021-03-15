f9fa6d1cf9a26ca0ed2aaa45e642f4c7c1c2110e
Brecht Vermeersch <brecht@Brechts-MacBook-Pro.local>
2021-03-15 15:31:04 +0100

    Add google analytics

--- src/main.js
+++ src/main.js
@@ -6,10 +6,15 @@ import 'normalize.css';
 import '@/assets/scss/styles.scss';
 
 import DirectusImg from "@/components/DirectusImg.vue"
+import VueAnalytics from 'vue-analytics'
 
 Vue.config.productionTip = false
 
 Vue.component('d-img', DirectusImg)
+
+Vue.use(VueAnalytics, {
+  id: 'G-C7RJHM2S7C'
+})
 
 new Vue({
   router,
